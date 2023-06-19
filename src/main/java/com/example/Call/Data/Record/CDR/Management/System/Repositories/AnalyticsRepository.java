1a4df83a43ae2fbb2cad94bddbdb6e95f48bd8c3
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-19 16:33:31 +0400

    Adding Analytics Repository

---
+++ src/main/java/com/example/Call/Data/Record/CDR/Management/System/Repositories/AnalyticsRepository.java
@@ -0,0 +1,7 @@
+package com.example.Call.Data.Record.CDR.Management.System.Repositories;
+
+import com.example.Call.Data.Record.CDR.Management.System.Models.Analytics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalyticsRepository extends JpaRepository<Analytics, Long> {
+}
