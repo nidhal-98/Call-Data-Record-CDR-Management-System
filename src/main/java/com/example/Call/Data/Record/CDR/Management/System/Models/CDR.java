5d246375a18a5419bbec718812ccf1ff2d96f535
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-19 11:19:52 +0400

    Adding CDR class

---
+++ src/main/java/com/example/Call/Data/Record/CDR/Management/System/Models/CDR.java
@@ -0,0 +1,26 @@
+package com.example.Call.Data.Record.CDR.Management.System.Models;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "/cdrs")
+public class CDR {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cdrId;
+
+    private String callerNumber;
+    private String receiverNumber;
+    private Integer duration;
+    private LocalDateTime timestamp;
+}
