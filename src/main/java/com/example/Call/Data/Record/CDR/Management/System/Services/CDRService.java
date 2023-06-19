8c90d200c8e549cad3f97eedda387de387a867b7
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-19 11:36:04 +0400

    Adding CDRService class

---
+++ src/main/java/com/example/Call/Data/Record/CDR/Management/System/Services/CDRService.java
@@ -0,0 +1,23 @@
+package com.example.Call.Data.Record.CDR.Management.System.Services;
+
+import com.example.Call.Data.Record.CDR.Management.System.Models.CDR;
+import com.example.Call.Data.Record.CDR.Management.System.Repositories.CDRRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CDRService {
+
+    @Autowired
+    CDRRepository cdrRepository;
+
+    public void registerCDR(CDR cdr){
+        cdrRepository.save(cdr);
+    }
+
+    public CDR getCDRByID(Long id){
+        return cdrRepository.findById(id).get();
+    }
+}
