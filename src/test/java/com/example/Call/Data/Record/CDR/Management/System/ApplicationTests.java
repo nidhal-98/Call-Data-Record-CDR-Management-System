dd7cda50d24cb0e9bd901a11e94da3255448006e
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-19 10:34:33 +0400

    Creating Packages

---
+++ src/test/java/com/example/Call/Data/Record/CDR/Management/System/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Call.Data.Record.CDR.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
