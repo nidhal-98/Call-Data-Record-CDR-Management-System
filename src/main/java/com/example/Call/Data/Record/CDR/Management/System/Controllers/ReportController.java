595ef7e749c9ac0e31f677ea6c53df81fcb11c26
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-21 12:23:12 +0400

    Adding Report Controller to deal with entered username

---
+++ src/main/java/com/example/Call/Data/Record/CDR/Management/System/Controllers/ReportController.java
@@ -0,0 +1,28 @@
+package com.example.Call.Data.Record.CDR.Management.System.Controllers;
+
+import com.example.Call.Data.Record.CDR.Management.System.Models.Report;
+import com.example.Call.Data.Record.CDR.Management.System.Services.CDRService;
+import com.example.Call.Data.Record.CDR.Management.System.Services.ReportService;
+import com.example.Call.Data.Record.CDR.Management.System.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/reports")
+public class ReportController {
+
+    @Autowired
+    ReportService reportService;
+
+    @GetMapping("/user_summary")
+    public ResponseEntity<Report> getUserSummary(
+            @RequestParam("username") String username
+    ) {
+        Report cdrSummary = reportService.getCDRSummaryByUsername(username);
+        return ResponseEntity.ok(cdrSummary);
+    }
+}
