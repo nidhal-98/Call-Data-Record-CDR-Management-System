57220426250c670dac189108d714ea5ccdab0180
nidhal-98 <nidhalsalim98@gmail.com>
2023-06-21 12:16:41 +0400

    I need it in Reporting.

--- src/main/java/com/example/Call/Data/Record/CDR/Management/System/Repositories/CDRRepository.java
+++ src/main/java/com/example/Call/Data/Record/CDR/Management/System/Repositories/CDRRepository.java
@@ -2,6 +2,7 @@ package com.example.Call.Data.Record.CDR.Management.System.Repositories;
 
 import com.example.Call.Data.Record.CDR.Management.System.Models.CDR;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -10,4 +11,6 @@ import java.util.List;
 @Repository
 public interface CDRRepository extends JpaRepository<CDR, Long> {
     List<CDR> findByCallerNumberAndTimestampBetween(String callerNumber, LocalDateTime timestampFrom, LocalDateTime timestampTo);
+
+    List<CDR> findByUserUsername(String username);
 }
